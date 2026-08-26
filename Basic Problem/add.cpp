47a9015c44f0040c3de80ee1c7d095c38c31471f
Munan95 <munankhan83@gmail.com>
2026-08-27 01:27:02 +0600

    Solved:Find single unique element

---
+++ Basic Problem/add.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int a = 15;
+    int b = 5;
+    int ans = a+b;
+
+    cout <<"Hello Bangladesh\n";
+    cout <<"Ans="<<ans<<"\n";
+    return 0; 
+}
